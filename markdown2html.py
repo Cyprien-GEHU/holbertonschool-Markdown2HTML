996faacedf88c24d94899ba1f3e8f61e51f0dd55
Cyprien <cypriengehu@example.com>
2025-08-12 10:29:46 +0200

    update: add a verification

--- markdown2html.py
+++ markdown2html.py
@@ -3,15 +3,16 @@
 import sys
 import os
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
 
-path_readme = sys.argv[1]
-path_html = sys.argv[2]
+    path_readme = sys.argv[1]
+    path_html = sys.argv[2]
 
-if not os.path.isfile(path_readme):
-    sys.stderr.write(f"Missing {path_readme}\n")
-    exit(1)
+    if not os.path.isfile(path_readme):
+        sys.stderr.write(f"Missing {path_readme}\n")
+        exit(1)
 
-exit(0)
+    exit(0)
