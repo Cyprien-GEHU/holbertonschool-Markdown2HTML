cf4c7554a5f35c0c209ff3350eb7864f3d1d2bc4
Cyprien <cypriengehu@example.com>
2025-08-12 10:18:38 +0200

    fix: correction of format print with stderr

--- markdown2html.py
+++ markdown2html.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 
 if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html")
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html" + '\n')
     exit(1)
 
 name = sys.argv[0]
@@ -12,11 +12,11 @@ path_readme = Path(sys.argv[1])
 path_html = Path(sys.argv[2])
 
 if not path_readme.exists():
-    print("Missing README.md")
+    sys.stderr.write("Missing README.md" + '\n')
     exit(1)
 
 if not path_html.exists():
-    print("Missing README.html")
+    sys.stderr.write("Missing README.html" + '\n')
     exit(1)
 
 exit(0)
