9bbb51e75f9df37f41d3c94ce7968f5b0733026d
Cyprien <cypriengehu@example.com>
2025-08-12 10:34:07 +0200

    update: create a function

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+""" markdown to html """
 import sys
 import os
 
-if __name__ == "__main__":
+
+def main():
+    """ the main function of the programm"""
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
@@ -14,5 +16,8 @@ if __name__ == "__main__":
     if not os.path.isfile(path_readme):
         sys.stderr.write(f"Missing {path_readme}\n")
         exit(1)
-
     exit(0)
+
+
+if __name__ == "__main__":
+    main()
