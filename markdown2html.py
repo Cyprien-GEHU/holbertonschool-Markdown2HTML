cfb1fc1c58fab9724768a43eeebca2563fe63bb9
Cyprien <cypriengehu@example.com>
2025-08-13 10:19:56 +0200

    update: we do a markdown with list (start with '-')

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,11 @@ import os
 
 def main():
     """ the main function of the programm"""
+
+    my_html = []
+    isList = False
+    tag = None
+
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
@@ -20,12 +25,35 @@ def main():
     with open(path_readme, 'r') as f_read, open(path_html, 'w') as f_wrote:
         for line in f_read:
             if line.startswith('#'):
+                if isList:
+                    my_html.append(f"</{tag}>")
+                    tag = None
+                    isList = False
+
                 num = 0
                 while num < len(line) and line[num] == "#":
                     num += 1
                 if 1 <= num <= 6 and len(line) > num and line[num] != "#":
                     title = line[num:].strip()
-                    f_wrote.write(f"<h{num}>{title}</h{num}>\n")
+                    my_html.append(f"<h{num}>{title}</h{num}>")
+
+            elif line.startswith('-'):
+                content = line[1:].strip()
+                if not isList:
+                    isList = True
+                    my_html.append("<ul>")
+                    tag = "ul"
+                elif isList and tag != "ul":
+                    my_html.append(f"</{tag}>")
+                    my_html.append("<ul>")
+                    tag = "ul"
+                my_html.append(f"\t<li>{content}</li>")
+
+        if isList:
+            my_html.append(f"</{tag}>")
+
+        for lh in my_html:
+            f_wrote.write(f"{lh}\n")
     exit(0)
 
 
